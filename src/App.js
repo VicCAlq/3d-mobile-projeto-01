d0da59c8f30f330cac18040d9993e93737af386f
arii-i <ariadne.maciel@nave.org.br>
2026-04-23 11:00:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { styles } from './src/styles/main';
+import { styles } from './styles/main';
 import { Text, View } from 'react-native';
-import Exemplo from './src/components/Exemplo';
+import Exemplo from './components/Exemplo';
+import Cabecalho from './components/Cabecalho';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [conteudo, setConteudo] = useState(
+    <Text style={styles.texto}>
+      Sou o conteúdo inicial
+    </Text>
+  )
+
   return (
     <View style={styles.container}>
-      <Exemplo titulo={'Elemento exemplo'}>
-        <Text style={styles.texto}>Aqui dentro deste elemento exemplo pode ter qualquer coisa</Text>
-      </Exemplo>
-      <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
+      <Cabecalho
+        paginaInicial={
+          <Text style={styles.texto}>
+            Em construção: Página inicial
+          </Text>
+        }
+        sobreEquipe={
+          <Text style={styles.texto}>
+            Em construção: Página da equipe
+          </Text>
+        }
+        mudarPagina={setConteudo}
+      ></Cabecalho>
+      {conteudo}
       <StatusBar style="auto" />
     </View>
   );
