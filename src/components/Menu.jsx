2c6c2bb4fb85e0aec98f3a38783c99ff69b27af0
arii-i <ariadne.maciel@nave.org.br>
2026-04-30 11:08:34 -0300

    cabecalho e menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { View, Pressable, Text } from 'react-native'
+
+const estilosCabecalho = StyleSheet.create({
+  cabecalho: {
+    backgroundColor: '#333',
+    marginBottom: "20px",
+    marginTop: "0px",
+    display: 'flex',
+    width: '100vw',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  botaoCabecalho: {
+    backgroundColor: "#359",
+    color: "white",
+    padding: '10px',
+    borderRadius: '10px',
+    margin: '10px',
+  }
+})
+
+export default function Menu({
+  paginaInicial
+}) {
+  const [selecionado, setSelecionado] = useState("principal")
+
+  return(<View style={estilosMenu.Menu}>
+    <Pressable 
+      style={() => [
+        estilosMenu.botaoMenu,
+        {
+          backgroundColor: selecionado === "principal"
+          ? "#395"
+          : "#359"
+        }
+      ]}
+      onPress={() => {
+        mudarPagina(paginaInicial)
+        setSelecionado("principal")
+      }}
+    >
+      Página principal
+    </Pressable>
+    <Pressable 
+      style={() => [
+        estilosMenu.botaoMenu,
+        {
+          backgroundColor: selecionado === "nmmmi"
+          ? "#395"
+          : "#359"
+        }
+      ]}
+      onPress={() => {
+        mudarPagina(sobreEquipe)
+        setSelecionado("sobreEquipe")
+      }}
+    >
+      Sobre a equipe
+    </Pressable>
+  </View>)
+}
